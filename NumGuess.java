bcedd3b63606967fdbed6532944e655548a1c05c
SadDucks <onionpupz@gmail.com>
2025-03-25 19:58:29 -0500

    Replaced redundant code with functions

--- NumGuess.java
+++ NumGuess.java
@@ -7,29 +7,12 @@
  *                         *
  ***************************/
 
-import java.util.Random;
-import java.util.Scanner;
+ import java.util.Random;
+ import java.util.Scanner;
+ 
+ public class NumGuess{
 
-public class NumGuess{
-    public static void main(String [] args){
-        //Intializing the Randomizer
-        Random answerRandom = new Random();
-
-        //Game Finish conditional & Attempt counter
-        Boolean done = false;
-        Boolean complete = false;
-        Boolean win = false;
-
-        //Game required variables
-        int userAttempt = 1;
-        int userGuess;
-
-        //Game Satistics
-        int gameCount = 1;
-        int wins = 0;
-        int losts = 0;
-
-        //Game Start
+    public static void welcomeScreen() {
         System.out.println("""
                 Welcome! This is the Number Guessing Game
                 I will pick a number between 1 and 100; your job is to try and guess it!\n
@@ -39,98 +22,121 @@ public class NumGuess{
                 I will tell you if your correct, too low, or too high
                 You get 7 guesses total
                 """);
-        System.out.println("Lets start!");
+    }
 
-        //Playing the game
-        while(!done) {
-            //Win & Lost conditional
-            System.out.println("Game " + gameCount);
-            Boolean lost = false;
-            //Intialzing scanner & obtaining answer
-            Scanner userGuessScanner = new Scanner(System.in);
-            int answer = (answerRandom.nextInt(100) + 1);
-       
-            while(!complete) {
-                System.out.print("Current Attempt Count: " + userAttempt + "\nWhat\'s your guess: ");
-                userGuess = userGuessScanner.nextInt(); {
-                    //Guess is Correct
-                    if(userGuess == answer) {
-                        complete = true;
-                        win = true;
-                        wins++;
-                        userGuessScanner.nextLine();
-                    }
-                    //Guess is too high!
-                    else if(userGuess > answer) {
-                        System.out.println("You answer is too high!");
-                        userAttempt++;
-                    }
-                    //Guess is too low!
-                    else {
-                        System.out.println("You answer is too low!");
-                        userAttempt++;
-                    }
-                    //Lost
-                    if (userAttempt > 7) {
-                        complete = true;
-                        lost = true;
-                        losts++;
-                        userGuessScanner.nextLine();
-                    }
-                }
-            }
-            //End screen lost
-            if (lost == true) {
-                System.out.print("Whoops! You lost the game. Would you like to try again? (Y/N) ");
-                String doneInput; 
-                doneInput = userGuessScanner.nextLine();
+    public static int getAnswer(int a) {
+        Random answerRandom = new Random();
+        return a = (answerRandom.nextInt(100) + 1);
+    }
 
-                if (doneInput.equals("Y")) {
-                    userAttempt = 1;
-                    complete = false;
-                    gameCount++;
-                }
+    public static int obtainGuess() {
+        Scanner userGuessScanner = new Scanner(System.in);
+        return userGuessScanner.nextInt();
+    }
 
-                else if (doneInput.equals("N")) {
-                    System.out.println("Thanks for playing");
-                    done = true;
-                }
-                else {
-                    System.out.println("Thanks for playing");
-                    done = true;
-                }
-            }
-            //End Screen Win
-            if (win == true) {
-                System.out.println("CORRECT! You won the game on attempt " + userAttempt);
-                System.out.print("Would you like to try again? (Y/N) ");
-                String doneInput; 
-                doneInput = userGuessScanner.nextLine();
-
-                if (doneInput.equals("Y")) {
-                    userAttempt = 1;
-                    complete = false;
-                    win = false;
-                    gameCount++;
-                }
-
-                else if (doneInput.equals("N")) {
-                    System.out.println("\nThanks for playing!");
-                    done = true;
-                    userGuessScanner.close();
-                }
-                else {
-                    System.out.println("\nThanks for playing!");
-                    done = true;
-                    userGuessScanner.close();
-                }
-            }
+    public static boolean gameComplete(boolean yes, int userGuess, int answer, int userAttempt) {
+        if(userGuess == answer) {
+            System.out.println("CORRECT! You won the game on attempt " + userAttempt);
+            return yes = true;
         }
-        
-        //Display Game Statistics
+        else {
+            return yes = false;
+        }
+    }
+
+    public static void highOrLow(int userGuess, int answer) {
+        if(userGuess > answer) {
+            System.out.println("Too high");
+        }
+        else {
+            System.out.println("Too low");
+        }
+    }
+
+    public static boolean playAgain(boolean done, String answer) {
+        System.out.print("Would you like to play again? (Y/N)");
+        Scanner userGuessScanner = new Scanner(System.in);
+
+        answer = userGuessScanner.nextLine();
+        if(answer.equalsIgnoreCase("Y")) {
+            return done = false;
+        }
+        else {
+            System.out.println("Thanks for playing!");
+            return done = true;
+        }
+    }
+
+    public static void statistics(int gameCount, int wins, int losts) {
         System.out.println("Game Statistics:\n");
         System.out.println("Total Games: " + gameCount);
         System.out.println("Wins: " + wins);
         System.out.println("Losts: " + losts);
     }
-}
+
+     public static void main(String [] args){
+ 
+         //Game Finish conditional & Attempt counter
+         Boolean done = false;
+         Boolean complete = false;
+         Boolean win = false;
+ 
+         //Game required variables
+         int userAttempt = 1;
+         int userGuess;
+ 
+         //Game Satistics
+         int gameCount = 1;
+         int wins = 0;
+         int losts = 0;
+ 
+         //Game Start
+         welcomeScreen();
+
+         System.out.println("Lets start!");
+ 
+         //Playing the game
+         while(!done) {
+             //Win & Lost conditional
+             System.out.println("Game " + gameCount);
+
+             //Intialzing scanner & obtaining answer
+             Scanner userGuessScanner = new Scanner(System.in);
+             int answer = getAnswer(0);
+        
+             while(!complete) {
+                 System.out.print("Current Attempt Count: " + userAttempt + "\nWhat\'s your guess: ");
+                 userGuess = obtainGuess(); {
+                     //Guess is Correct
+                     if(gameComplete(true, userGuess, answer, userAttempt)) {
+                         complete = true;
+                         win = true;
+                         wins++;
+                     }
+                     //Guess is too high!
+                     else {
+                         highOrLow(userGuess, answer);
+                         userAttempt++;
+                     }
+
+                     //Lost
+                     if (userAttempt > 7) {
+                        System.out.print("Whoops! You lost the game. Would you like to try again? (Y/N) ");
+                         complete = true;
+                         losts++;
+                     }
+                 }
+             }
+             //Play Again
+             done = playAgain(false, "");
+             if (!done) {
+                    gameCount++;
+                    userAttempt = 1;
+                    complete = false;
+                }
+         }
+         
+         //Display Game Statistics
+         statistics(gameCount, wins, losts);
+     }
+ }
